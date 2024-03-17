3f1860ab90295ebbb3feffac7afa67af84f912cc
Leozerbib <116356061+Leozerbib@users.noreply.github.com>
2024-03-17 18:04:30 +0100

    Update Utilisateur.java

--- src/main/java/com/example/AquaStorageApi/Entity/Utilisateur.java
+++ src/main/java/com/example/AquaStorageApi/Entity/Utilisateur.java
@@ -1,20 +1,26 @@
 package com.example.AquaStorageApi.Entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter@Setter
 @Data
 @Entity
 @AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "utilisateur")
 public class Utilisateur{
+	
+	@Id
+	private String Id;
+	
+	@Column(name = "nom")
+	private String nom;
 	
 	
 }
