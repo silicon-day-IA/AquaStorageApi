4f2e00abbbfdb31249eac7d8b56f9dd4dfb1247a
Leozerbib <116356061+Leozerbib@users.noreply.github.com>
2024-03-17 15:51:28 +0100

    Initial commit

---
+++ src/test/java/com/example/AquaStorageApi/AquaStorageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AquaStorageApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaStorageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
